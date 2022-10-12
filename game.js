a26ecc8309e211c15911bbce75f91b776092c08b
damskw <damian.skwierawski@gmail.com>
2022-10-13 00:31:15 +0200

    create base javascript handlers

--- game.js
+++ game.js
@@ -1,7 +1,50 @@
-initGame();
+const mainGameContainer = document.querySelector(".main-game-container");
+const menuContainer = document.querySelector(".menu-container");
+const menuBar = document.querySelector(".menu-bar");
+const menuOptions = document.querySelectorAll(".menu-content > div");
+const menuTitle = document.querySelector("#menu-bar");
+const gameContent = document.querySelector(".game-content");
+const statusBar = document.querySelector(".top-bar-game-content");
+const petBackground = document.querySelector(".pet-game-content");
+const petBody = document.querySelector(".pet-body");
+const bottomBar = document.querySelector(".bottom-bar-game-content");
+const actionButtons = document.querySelectorAll(".action-button");
+const inventoryContainer = document.querySelector(".inventory-container");
+const inventoryBar = document.querySelector(".inventory-bar");
+const inventoryBarTitle = document.querySelector("#inventory-bar");
+const inventoryItems = document.querySelectorAll(".inventory-item");
+
+
+
+
+const game = {
+    init: function() {
+        initGame();
+        initDragAndDrop();
+    },
+    running: function() {
+        setPetHunger();
+    },
+    end: function() {
+        endGame();
+    }
+}
+
+function initDragAndDrop () {
+
+}
 
 function initGame() {
-
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
+
+function setPetHunger() {
+
+}
+
+function endGame() {
+
+}
+
+game.init()
