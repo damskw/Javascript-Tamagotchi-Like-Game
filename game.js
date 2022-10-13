a6dd6a32fbe4dcbbb1f26637749ef7c4494e70d5
Julia <jbutkievicz@gmail.com>
2022-10-13 21:37:23 +0200

    Buttons, time and pet upgrade

--- game.js
+++ game.js
@@ -5,3 +5,11 @@ function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
+
+function refreshTime() {
+    const timeDisplay = document.getElementById("time");
+    const dateString = new Date().toLocaleString();
+    const formattedString = dateString.replace(", ", " - ");
+    timeDisplay.textContent = formattedString;
+  }
+    setInterval(refreshTime, 1000);
